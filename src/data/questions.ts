ad3bad243489534c2cfda4827fcf2c5cac837754
Isaias Pavon <isaiaspavon22@gmail.com>
2025-04-05 23:06:30 -0400

    question file updated. Needs more questions

---
+++ src/data/questions.ts
@@ -0,0 +1,19 @@
+// This file contains the questions for the gym purity test.
+// The questions are stored in an array of strings.
+// Each question is a string.
+// The questions are related to gym culture and fitness.
+// The questions are meant to be humorous and light-hearted.
+// The questions are not meant to be taken seriously.
+// The questions are meant to be fun and entertaining.
+
+const gymQuestions: string[] = [
+    "Dry-scooped pre-workout?",
+    "Skipped leg day more than twice in a row?",
+    "Lifted shirt in gym mirror for a pump check?",
+    "Posted shirtless gym selfie on IG?",
+    "Tried to flirt mid-set?",
+    // Add more later!
+  ];
+  
+  export default gymQuestions;
+  
